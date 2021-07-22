0ff979b1bce4db919a0af05a03927b3d856061ab
lazio <diniovskiy@gmail.com>
2021-07-22 13:03:38 +0300

    Revert "test deploy"
    This reverts commit 36d3866b7dd14649f1f3cf8c4dd235ec38ef4849.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import s from './Header.module.scss';
-import IconLogo from '../assets/IconLogo/IconLogo';
-import IconFork from '../assets/IconFork/IconFork';
+
+import s from './Header.module.scss'
+import IconLogo from "../assets/IconLogo/IconLogo";
+import IconFork from "../assets/IconFork/IconFork";
+
 
 const Header = () => {
+
+
   return (
-    <header className={s.wrap}>
-      <div className={s.logo}>
-        <div className={s.icon}>
-          <IconLogo />
-        </div>
-        <p>Statistics of your github reposs</p>
-      </div>
+   <header className={s.wrap}>
 
-      <div className={s.fork}>
-        <IconFork />
-        <a href={'https://github.com/Halo-Lab/STTS'}>
-          <p>Fork me on github</p>
-        </a>
-      </div>
-    </header>
-  );
-};
+     <div className={s.logo}>
+       <div className={s.icon}>
+         <IconLogo/>
+       </div>
+       <p>Statistics of your github repos</p>
+     </div>
 
-export default Header;
+     <div className={s.fork}>
+       <IconFork/>
+      <a href={'https://github.com/Halo-Lab/STTS'}>
+        <p>Fork me on github</p>
+      </a>
+     </div>
+
+   </header>
+  )
+}
+
+export default Header
